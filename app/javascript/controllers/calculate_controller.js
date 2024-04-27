ec7bc5b48eddaed51dff45367669072a043b4569
yizhu zong <agnes.lain@gmail.com>
2024-04-27 23:24:45 +0200

    add multiplication + division functions

--- app/javascript/controllers/calculate_controller.js
+++ app/javascript/controllers/calculate_controller.js
@@ -1,41 +1,68 @@
 import { Controller } from "@hotwired/stimulus"
 
+
 export default class extends Controller {
   static targets = ["calculateZone", "answer", "showResult"]
 
 
   addition () {
     this.calculateZoneTarget.innerHTML=""
-    for (let i=0; i < 5 ;i++) {
+    for (let i=0; i < 10 ;i++) {
       const a = Math.floor(Math.random() * 100);
       const b = Math.floor(Math.random() * 100);
       let answer = a + b
       this.calculateZoneTarget.innerHTML +=
-        `<li data-calculate-target="showResult">
+      `<li data-calculate-target="showResult">
           ${a} + ${b} = <input type="text" id=${answer} name="response" placeholder="0" data-calculate-target="answer" >
-        </li>`
-    }
+          </li>`
+        }
   }
 
   soustraction () {
     this.calculateZoneTarget.innerHTML=""
-    for (let i=0; i < 5 ;i++) {
+    for (let i=0; i < 10 ;i++) {
       const a = Math.floor(Math.random() * 100);
       const b = Math.floor(Math.random() * 100);
       if (a >= b) {
         let answer = a - b
         this.calculateZoneTarget.innerHTML +=
-          `<li data-calculate-target="showResult">
-            ${a} - ${b} = <input type="text" id=${answer} name="response" placeholder="0" data-calculate-target="answer" >
-          </li>`
+        `<li data-calculate-target="showResult">
+        ${a} - ${b} = <input type="text" id=${answer} name="response" placeholder="0" data-calculate-target="answer" >
+        </li>`
       } else {
         let answer = b - a
         this.calculateZoneTarget.innerHTML +=
-          `<li data-calculate-target="showResult">
-            ${b} - ${a} = <input type="text" id=${answer} name="response" placeholder="0" data-calculate-target="answer" >
-          </li>`
+        `<li data-calculate-target="showResult">
+        ${b} - ${a} = <input type="text" id=${answer} name="response" placeholder="0" data-calculate-target="answer" >
+        </li>`
       }
 
+    }
+  }
+
+  multiplication () {
+    this.calculateZoneTarget.innerHTML=""
+    for (let i=0; i < 10 ;i++) {
+      const a = Math.floor(Math.random() * 9 + 1);
+      const b = Math.floor(Math.random() * 9 + 1);
+      let answer = a * b
+      this.calculateZoneTarget.innerHTML +=
+      `<li data-calculate-target="showResult">
+        ${a} * ${b} =  <input type="text" id=${answer} name="response" placeholder="0" data-calculate-target="answer" >
+      </li>`
+    }
+  }
+
+  division () {
+    this.calculateZoneTarget.innerHTML=""
+    for (let i=0; i < 10 ;i++) {
+      const a = Math.floor(Math.random() * 9 + 1);
+      const b = Math.floor(Math.random() * 9 + 1);
+      let answer = a * b
+      this.calculateZoneTarget.innerHTML +=
+      `<li data-calculate-target="showResult">
+        ${answer} / ${a} =  <input type="text" id=${b} name="response" placeholder="0" data-calculate-target="answer" >
+      </li>`
     }
   }
 
@@ -47,10 +74,30 @@ export default class extends Controller {
       let ans = parseInt(answer.value)
       // console.log(ans)
       if (result === ans){
-        answer.parentNode.insertAdjacentHTML('beforeend', "✅")
+        answer.parentNode.insertAdjacentHTML('beforeend', " ✅")
       } else {
-        answer.parentNode.insertAdjacentHTML('beforeend', "❌")
+        answer.parentNode.insertAdjacentHTML('beforeend', " ❌")
       }
     }
   }
 }
+
+// function math(num_max, quantity, formula) {
+//   for (let i=0; i < quantity ;i++) {
+//     const a = Math.floor(Math.random() * num_max);
+//     const b = Math.floor(Math.random() * num_max);
+//     let answer = 0
+//     switch(formula) {
+//       case '+':
+//         answer = a + b
+//       case '-':
+//         a >= b ? answer = a - b : answer = b - a
+//       case '*':
+//         answer = a * b
+//       case "/":
+//         a >= b ? answer = a / b : answer = b / a
+//     }
+//     return [a, b, answer]
+//   }
+
+// }
